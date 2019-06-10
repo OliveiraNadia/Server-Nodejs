9ab7e46310b9c8ae80ade73537a209269a6d4c63
Nadia Regina de Oliveira <nadia.oliveira@bgmrodotec.com.br>
2019-06-10 13:25:17 -0300

    Criando base de dados com node

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const http = require('http');
+const app = require('./src/config/custom-express');
 
-const servidor = http.createServer(function (req, resp) {
 
-    let html = '';
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body>
-            </html>
-        `;
-    } else if (req.url == '/livros') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de livros </h1>
-                </body>
-            </html>
-        `;
-    }
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000');
 
-    // outros else-if
-
-    resp.end(html);
 });
-servidor.listen(3000);
+
+
+
+
+// const http = require('http');
+
+// const servidor = http.createServer(function (req, resp) {
+
+//     let html = '';
+//     if (req.url == '/') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Casa do Código </h1>
+//                 </body>
+//             </html>
+//         `;
+//     } else if (req.url == '/livros') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Listagem de livros </h1>
+//                 </body>
+//             </html>
+//         `;
+//     }
+
+//     // outros else-if
+
+//     resp.end(html);
+// });
+// servidor.listen(3000);
